34890456f228f37319c0eb5065fe4359efe68b03
ManziniArchives <manziniarchives@gmail.com>
2025-12-18 20:59:33 +0200

    feat: add LumaSpin UI component for a CSS-animated loader.

--- src/components/ui/luma-spin.tsx
+++ src/components/ui/luma-spin.tsx
@@ -1,9 +1,9 @@
 export const Component = () => {
-    return (
-        <div className="relative w-[65px] aspect-square" >
-            <span className="absolute rounded-[50px] animate-loaderAnim shadow-[inset_0_0_0_3px] shadow-gray-800 dark:shadow-gray-100" />
-            <span className="absolute rounded-[50px] animate-loaderAnim animation-delay shadow-[inset_0_0_0_3px] shadow-gray-800 dark:shadow-gray-100" />
-            <style jsx > {`
+  return (
+    <div className="relative w-[65px] aspect-square" >
+      <span className="absolute rounded-[50px] animate-loaderAnim shadow-[inset_0_0_0_3px] shadow-gray-800 dark:shadow-gray-100" />
+      <span className="absolute rounded-[50px] animate-loaderAnim animation-delay shadow-[inset_0_0_0_3px] shadow-gray-800 dark:shadow-gray-100" />
+      <style jsx > {`
         @keyframes loaderAnim {
           0% {
             inset: 0 35px 35px 0;
@@ -40,6 +40,6 @@ export const Component = () => {
           animation-delay: -1.25s;
         }
       `}</style>
-            < /div>
-            );
+    </div>
+  );
 };
